3b285fa38c6b7acd286b33051bc465b6869ea19f
Youssif Hany <youssifhany962@gmail.com>
2022-04-04 21:42:47 +0200

    Initial Commit

---
+++ CalorieHut/CalorieHutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieHutApp.swift
+//  CalorieHut
+//
+//  Created by Youssif Hany on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalorieHutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieHut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalorieHut
+//
+//  Created by Youssif Hany on 04/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
