981194863839765ccefd634cca6f380e103ca564
Youssif Hany <youssifhany962@gmail.com>
2022-04-07 02:47:50 +0200

    Meal  Model

--- CalorieHut/Model/Meal.swift
+++ CalorieHut/Model/Meal.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Meal:Identifiable,Codable{
+    
+    let id:String
+    let name:String
+    var kcal:Float
+    var quantity:Int
+    var isFav:Bool
+
+    init(id:String = UUID().uuidString,name:String,kcal:Float,quantity:Int,isFav:Bool){
+        self.id=id
+        self.name=name
+        self.kcal=kcal
+        self.quantity=quantity
+        self.isFav=isFav
+    }
+    
+    func updateComplete()->Meal{
+        return Meal(id: id, name: name, kcal: kcal, quantity: quantity,isFav: isFav)
+    }
+}
