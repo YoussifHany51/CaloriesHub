8e4ee9d52a3987544edb5b18ff2a88213f8fcbb6
Youssif Hany <youssifhany962@gmail.com>
2022-04-08 00:52:58 +0200

    ProgressRing

--- CalorieHut/Views/ProgressRingView.swift
+++ CalorieHut/Views/ProgressRingView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct ProgressRingView: View {
+    
     @State var progress = 0.0
+    @EnvironmentObject var vm : MealViewModel
     
     var body: some View {
         ZStack{
@@ -18,7 +20,7 @@ struct ProgressRingView: View {
                 .opacity(0.1)
             
             Circle()
-                .trim(from: 0.0, to: min(progress , 1.0))
+                .trim(from: 0.0, to: min(Double(vm.getCountKcal()) / 200 , 1.0))
                 .stroke(AngularGradient(
                 gradient:Gradient(colors: [Color.blue,Color.blue]),
                 center: .center),
@@ -27,18 +29,20 @@ struct ProgressRingView: View {
                 lineCap: .round,
                 lineJoin: .round))
                 .rotationEffect(Angle(degrees: 270))
-                .animation(.easeInOut(duration: 1.0), value: progress)
+                .animation(.easeInOut(duration: 1.0), value: Double(vm.getCountKcal()) / 200)
         }
         .frame(width:230,height: 250)
         .padding()
-        .onAppear{
-            progress = 0.5
-        }
+//        .onAppear{
+//            //progress = 0.5
+//           progress = Double(vm.getCountKcal()) / 200
+//        }
     }
 }
 
 struct ProgressRingView_Previews: PreviewProvider {
     static var previews: some View {
         ProgressRingView()
+            .environmentObject(MealViewModel())
     }
 }
