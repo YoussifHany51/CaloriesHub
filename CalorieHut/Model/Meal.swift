934544ae98786b4661d3d0aedd47e9e9235e71db
Youssif Hany <youssifhany962@gmail.com>
2022-04-09 02:50:45 +0200

    Meal Model

--- CalorieHut/Model/Meal.swift
+++ CalorieHut/Model/Meal.swift
@@ -12,18 +12,20 @@ struct Meal:Identifiable,Codable{
     let id:String
     let name:String
     var kcal:Float
-    var quantity:Int
+    let quantity:Int
+    let unit:String
     var isFav:Bool
 
-    init(id:String = UUID().uuidString,name:String,kcal:Float,quantity:Int,isFav:Bool){
+    init(id:String = UUID().uuidString,name:String,kcal:Float,quantity:Int,unit:String,isFav:Bool){
         self.id=id
         self.name=name
         self.kcal=kcal
         self.quantity=quantity
+        self.unit=unit
         self.isFav=isFav
     }
     
     func updateComplete()->Meal{
-        return Meal(id: id, name: name, kcal: kcal, quantity: quantity,isFav: isFav)
+        return Meal(id: id, name: name, kcal: kcal, quantity: quantity,unit: unit,isFav: !isFav)
     }
 }
---
+++ CalorieHut/Views/FavoriteMealView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteMealView.swift
+//  CalorieHut
+//
+//  Created by Youssif Hany on 09/04/2022.
+//
+
+import SwiftUI
+
+struct FavoriteMealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteMealView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteMealView()
+    }
+}
