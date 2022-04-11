968010e79a12860fcbd4c7d5f85c72f317d2e7c2
Youssif Hany <youssifhany962@gmail.com>
2022-04-12 00:12:55 +0200

    Category List

--- CalorieHut/Views/CategoryGridView.swift
+++ CalorieHut/Views/CategoryGridView.swift
@@ -8,13 +8,65 @@
 import SwiftUI
 
 struct CategoryGridView: View {
+    
+   var columns =
+    Array(repeating: GridItem(.flexible(),spacing: 20),count:2)
+    
+//   @State var showMealSheet:Bool = false
+   @State var categoryName:String = ""
+   @State var catImage:String = ""
+    
+    @EnvironmentObject var vm : MealViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        NavigationView {
+            ZStack {
+                    Color(.systemBrown).opacity(0.5)
+                        .ignoresSafeArea()
+                
+                    ScrollView {
+                        LazyVGrid(columns:columns){
+                            ForEach(vm.categorylist){ cat in
+                                Button{
+                                    vm.showMealSheet.toggle()
+                                    categoryName = cat.title
+                                    catImage = cat.image
+                                }label: {
+                                ZStack{
+                                    Image(cat.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        
+                                    VStack(spacing: 15){
+                                        Text(cat.title)
+                                            .foregroundColor(.white)
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                    }
+                                    .padding()
+                                }
+                                .cornerRadius(20)
+                                
+                            }
+                                .sheet(isPresented: $vm.showMealSheet, content: {
+                                VStack{
+                            MealsListView(name: categoryName, image: catImage)
+                                }
+                                    })
+                        }
+                        .padding()
+                        
+                    }
+            }
+            }
+            .navigationTitle("Add new meal")
+        }
 }
 
 struct CategoryGridView_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryGridView()
+            CategoryGridView()
+            .environmentObject(MealViewModel())
     }
+  }
 }
---
+++ CalorieHut/Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  CalorieHut
+//
+//  Created by Youssif Hany on 10/04/2022.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
