e5edfd068a265bb8b2009b3209113b232f088c34
Youssif Hany <youssifhany962@gmail.com>
2022-04-12 17:43:19 +0200

    Add Fav meal

--- CalorieHut/Views/FavoriteMealView.swift
+++ CalorieHut/Views/FavoriteMealView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct FavoriteMealView: View {
+    @EnvironmentObject var vm : MealViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if vm.favoriteMeals.isEmpty{
+            Text("No Favorite meals !!")
+                .font(.largeTitle)
+        }
+        else {
+            NavigationView {
+                ZStack {
+                    Color.brown.opacity(0.5)
+                        .ignoresSafeArea()
+                    VStack {
+                        List{
+                            ForEach(vm.favoriteMeals){ fav in
+                                HStack{
+                                    Text(fav.name)
+                                    
+                                    HStack {
+                                        Text("\(fav.kcal.description)")
+                                            .foregroundColor(.red)
+                                        Text("kcal")
+                                        Text("per")
+                                        Text("\(fav.quantity)")
+                                        Text(fav.unit)
+                                    }
+                                    Spacer()
+                                    
+                                    Button{
+                                        vm.addMeal(name: fav.name,
+                                                   kcal: fav.kcal,
+                                                   quantity: fav.quantity,
+                                                   unit: fav.unit)
+                                        vm.setCountKcal(count: fav.kcal)
+                                        print("Added")
+                                    }label: {
+                                        Image(systemName: "plus")
+                                            .padding()
+                                    }
+                                    .buttonStyle(PlainButtonStyle())
+                                }
+                            }
+                            .onDelete(perform: vm.deleteFavMeal)
+                        }
+                        .navigationTitle("Favorite Meals")
+                    }
+                }
+            }
+        }
     }
 }
 
 struct FavoriteMealView_Previews: PreviewProvider {
     static var previews: some View {
         FavoriteMealView()
+            .environmentObject(MealViewModel())
     }
 }
