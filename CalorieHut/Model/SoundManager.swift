09cc4bee61b12132daa37a762a95d6fa11da7cb0
Youssif Hany <youssifhany962@gmail.com>
2022-04-24 23:08:05 +0200

    Sound Added

--- CalorieHut/Model/SoundManager.swift
+++ CalorieHut/Model/SoundManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import AVKit
+
+class SoundManager{
+    
+    static let instance = SoundManager()
+    
+    var player : AVAudioPlayer?
+    
+    func playSound(){
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: ".mp3") else{ return }
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }catch let error{
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+        
+    }
+}
